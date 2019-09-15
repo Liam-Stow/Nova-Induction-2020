aa568232e26203831476b40fdece27af272a0d6c
Liam Stow <liam.stow@outlook.com>
2019-09-15 16:08:09 +1000

    Created publisher

---
+++ src/turtles/scripts/publisher.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""Does a thing.
+
+More stuff.
+"""
+
+import rospy
+import random
+from turtles.msg import turtleQuality
+
+if __name__ == "__main__":
+    pub = rospy.Publisher("turtles", turtleQuality)
+    rospy.init_node('publisherNode')
+    rate = rospy.Rate(10)
+    index = 0
+    while not rospy.is_shutdown():
+        turtle_msg = turtleQuality()
+        turtle_msg.index = index
+        turtle_msg.quality = random.randint(1, 10)
+        pub.publish(turtle_msg)
+        rate.sleep()
