024e5b78e095b4c9f760aba4706e765b18a55976
Liam Stow <liam.stow@outlook.com>
2019-09-15 20:33:32 +1000

    Improved comments in publisher

--- src/turtles/scripts/publisher.py
+++ src/turtles/scripts/publisher.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
-"""Does a thing.
+"""Generates and publishes turtles.
 
-More stuff.
+Date last updated: 15/9/19 by Liam Stow
+
+Purpose: Completes the software induction task for 2019/2020
+Subscribed topic/s: N/A
+Published topic/s: "turtles"
 """
 
 import rospy
@@ -10,14 +14,25 @@ import random
 from turtles.msg import turtleQuality
 
 if __name__ == "__main__":
+    
     pub = rospy.Publisher("turtles", turtleQuality, queue_size=10)
     rospy.init_node('publisherNode')
+    
+    # Loop at 5Hz
     rate = rospy.Rate(5)
+
+    # Count the turtles as we go
     index = 0
     while not rospy.is_shutdown():
+
+        # Make a random turtle
         turtle_msg = turtleQuality()
         turtle_msg.index = index
         turtle_msg.quality = random.randint(1, 10)
+
+        # Publish the turtle
         pub.publish(turtle_msg)
+
+        # Wait a moment in each loop interation
         index += 1
         rate.sleep()
