c3a2929f8fc3233ba91b9770183d25bf48990f3d
Liam Stow <liam.stow@outlook.com>
2019-09-15 17:41:19 +1000

    rate changed to 5Hz + index works

--- src/turtles/scripts/publisher.py
+++ src/turtles/scripts/publisher.py
@@ -12,11 +12,12 @@ from turtles.msg import turtleQuality
 if __name__ == "__main__":
     pub = rospy.Publisher("turtles", turtleQuality)
     rospy.init_node('publisherNode')
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(5)
     index = 0
     while not rospy.is_shutdown():
         turtle_msg = turtleQuality()
         turtle_msg.index = index
         turtle_msg.quality = random.randint(1, 10)
         pub.publish(turtle_msg)
+        index += 1
         rate.sleep()
